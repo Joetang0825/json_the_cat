23356833b025b48f79f576c8df7c77d7f70373db
Joe Tang <joetang81@gmail.com>
2022-02-25 21:58:44 -0500

    index.js test

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,24 @@
 const request = require('request');
-const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + process.argv[2];
 
+const fetchBreedDescription = function (breedName, callback) {
+  const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-
-
-request(URL, (error, response, body) => {
-  if (error) {
-    return console.log(error);
-  }
-
-  if (response.statusCode === 200) {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log('Breed not found');
-    } else {
-      console.log(data[0].description);
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     }
-  }
-});
+
+    if (!error) {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback('Breed not found', null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+
+};
+
+
+module.exports = { fetchBreedDescription };
